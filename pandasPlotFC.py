87c7e127f26f0c5fd5194dc8c8f1ad5a8b7691de
chfreem <freemche@oregonstate.edu>
2018-05-16 11:56:51 +0300

    Added next button to screen, not part of header list

--- pandasPlotFC.py
+++ pandasPlotFC.py
@@ -1,12 +1,6 @@
 #  This is a demo program, written by team Scorpius for CS 467, Capstone
 #  Team members:  John Carrabino, Aaron Peressini, Cheryl Freeman
 #  
-#  The intent is, that this file will read in data from a hard-coded
-#  data file, prompt the user for some data (possibly graph title
-#  and/or axis titles), and then graph the data.  The goal is to
-#  graph the data using pandas, using different columns of the data
-#  file to do a scatter plot, a line plot, and a bar plot.
-#  
 #  Kivy is used to prompt the user.  The kivy file that accompanies this
 #  contains the gui info.
 from kivy.app import App
@@ -18,6 +12,7 @@ from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 import matplotlib.pyplot as plt
 import numpy as np
@@ -26,13 +21,8 @@ import os
 import sys
 from plotter_class import PyPlotter
 
-#class Button(Widget):
- #   pass
-
-
 class GraphSession(Widget):
     
-    #asdf = PyPlotter()
     welcome_text = ("Welcome to Scorpius Plotter, a graphing application."
                     " You can select your input data, customize, preview, and"
                     " save your graphs.")
@@ -50,8 +40,6 @@ class GraphSession(Widget):
         self.plotter = PyPlotter()
         super(GraphSession, self).__init__()
 
-    #graph_now =  ObjectProperty(None)
-
     def assign_header(self, btn):
         if self.cur_axis == 'x':
             self.x_axis = btn.text
@@ -63,18 +51,23 @@ class GraphSession(Widget):
             self.ids.sm.current = 'screenY'
 
     def header_choices(self, axis):
-        #self.headers = [x.strip() for x in self.headers if len(x.strip()) > 0]
         self.cur_axis = axis
-        layout = GridLayout(cols=2)
+        chooseAxisScreen = FloatLayout()
+        headerButtons = GridLayout(cols=2, size_hint_y=0.3, pos_hint={'top': 0.9})
+        chooseAxisScreen.add_widget(headerButtons)
+        nextButton = Button(text = 'Next', size_hint_y=0.15, size_hint_x=0.2, pos_hint={'x': 0.7, 'y': 0.25}) 
+#        nextButton = Button(text = 'Next', size_hint_y=0.15, size_hint_x=0.2, pos_hint=(0.7, 0.25))
+        chooseAxisScreen.add_widget(nextButton)
         print self.headers
         for header in self.headers:
             btn = Button(text=header)
             btn.bind(on_press=self.assign_header)
-            layout.add_widget(btn)
-        layout.add_widget(Button(text='Next', color=[0.313, 1, 0.815, 1], on_press=lambda *args: self.popup.dismiss()))
-        content = layout
+            headerButtons.add_widget(btn)
+        headerButtons.add_widget(Button(text='Next', color=[0.313, 1, 0.815, 1], on_press=lambda *args: self.popup.dismiss()))
+#        content = headerButtons
+        content = chooseAxisScreen
         title = 'Select your ' + self.cur_axis + '-axis'
-        self.popup = Popup(content=content, title=title, size_hint=(0.9, 0.9))
+        self.popup = Popup(content=content, title=title, size_hint=(1.0, 1.0))
         self.popup.open()
 
     def print_axis(self, axis):
@@ -85,11 +78,9 @@ class GraphSession(Widget):
 
 
     def create_graph(self, buttonClicked):  
-        #content = Button(text=self.filename)
-        #popup = Popup(content=content)
-        #popup.open()
         df = self.readFile(self)
         print df
+
 #  I'd prefer to use the following line, but it doesn't work for
 #  some reason.  I don't know why.  So using the text attribute
 #  instead seems to be a workaround that functions.
@@ -111,10 +102,6 @@ class GraphSession(Widget):
             pass
 
     def readFile(self, df):
-        
-        #content = FileChooserIconView(path=self.path)
-        #popup = Popup(content=content)
-        #popup.open()
         return pd.read_csv(self.filename)
     
 class GraphApp(App):
